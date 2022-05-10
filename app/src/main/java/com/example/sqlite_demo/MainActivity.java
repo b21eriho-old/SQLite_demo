e839efdbb00848a76fde13ab27aacc880d279f22
Erik Holmberg <b21eriho@student.his.se>
2022-05-10 09:20:45 +0200

    fetch json

--- app/src/main/java/com/example/sqlite_demo/MainActivity.java
+++ app/src/main/java/com/example/sqlite_demo/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.sqlite_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("==>", json);
     }
 }
